3a4f3036ca3d2bccb92163e839dd1edeecaafcbe
Kofi <qouphieligal@gmail.com>
2024-04-22 22:08:16 -0700

    AirBnB clone - MySQL

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,7 +9,6 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
-
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
@@ -47,36 +46,4 @@ class FileStorage:
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
-    def reload(self):
-        """Loads storage dictionary from file."""
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
-        try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
-            pass
-        except json.decoder.JSONDecodeError:
-            pass
-
-    def delete(self, obj=None):
-        """
-         Delete obj from __objects if it’s inside - if obj is equal to None,
-           the method should not do anything
-        """
-        if obj is None:
-            return
-        obj_to_del = f"{obj.__class__.__name__}.{obj.id}"
-
-        try:
-            del FileStorage.__objects[obj_to_del]
-        except AttributeError:
-            pass
-        except KeyboardInterrupt:
-            pass
+    def reload
