34454cdefc0d3222cc268780c0c8502ef41980fa
samuelkofiannan <qouphieligal@gmail.com>
2024-05-22 15:06:08 -0700

    update

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,10 +1,9 @@
-
 #!/usr/bin/python3
 # Fabfile to delete out-of-date archives.
 import os
 from fabric.api import *
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["34.232.77.231", "107.21.39.244"]
 
 
 def do_clean(number=0):
